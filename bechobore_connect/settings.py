f5a3b6b6f02c13ded649169061c8b2374b91e242
Amir Seid <sj3398580@gmail.com>
2026-05-21 00:54:57 +0300

    Finalizing

--- bechobore_connect/settings.py
+++ bechobore_connect/settings.py
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'bechobore_connect.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Render ላይ ካልሆንክ በኮምፒውተርህ ላይ ያለውን sqlite3 እንዲጠቀም ያደርገዋል
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
