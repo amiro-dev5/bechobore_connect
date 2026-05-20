dec2a55f8a0ac6b6b899579be680605f48139a2e
Amir Seid <sj3398580@gmail.com>
2026-05-20 07:43:04 +0300

    Fix python version error in requirements

--- bechobore_connect/settings.py
+++ bechobore_connect/settings.py
@@ -111,10 +111,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 LOGIN_URL = 'home'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
