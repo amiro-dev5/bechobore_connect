651f98b76f461cf3d8078afa8b0e6deb9f9357d7
Amir Seid <sj3398580@gmail.com>
2026-05-20 08:28:21 +0300

    Fix python version error in requirements

--- bechobore_connect/settings.py
+++ bechobore_connect/settings.py
@@ -74,10 +74,11 @@ WSGI_APPLICATION = 'bechobore_connect.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Render ላይ ካልሆንክ በኮምፒውተርህ ላይ ያለውን sqlite3 እንዲጠቀም ያደርገዋል
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
@@ -112,6 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 import os
+import dj_database_url
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
